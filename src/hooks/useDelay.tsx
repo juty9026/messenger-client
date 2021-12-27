7ba66ecb9542c79b3fd11e10c7d5f931ecf3da7f
Minu <juty9026@gmail.com>
2021-12-27 11:17:30 +0900

    Create useDelay hook

---
+++ src/hooks/useDelay.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+
+interface DelayState<T> {
+  data: T[];
+  finish: boolean;
+}
+interface DelayApi {
+  reset: () => void;
+}
+const useDelay = <T extends unknown>(data: T[]): [DelayState<T>, DelayApi] => {
+  const [delayedData, setDelayedData] = useState<T[]>([]);
+  const [finish, setFinish] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (data.length === 0) {
+      return;
+    }
+
+    let timer: NodeJS.Timeout;
+
+    if (delayedData.length === data.length) {
+      setFinish(true);
+    } else {
+      setFinish(false);
+      const newMessages = data.slice(0, delayedData.length + 1);
+      timer = setTimeout(() => setDelayedData(newMessages), 1000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [data, delayedData, setDelayedData]);
+
+  const reset = useCallback(() => {
+    setDelayedData([]);
+    setFinish(false);
+  }, [setDelayedData, setFinish]);
+
+  return [{ data: delayedData, finish }, { reset }];
+};
+
+export default useDelay;
