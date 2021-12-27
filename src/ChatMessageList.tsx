bdf9e486afbe8de121fc4801b4d746df93f35502
Minu <juty9026@gmail.com>
2021-12-27 11:52:30 +0900

    Feat delayed message & scroll to bottom

--- src/ChatMessageList.tsx
+++ src/ChatMessageList.tsx
@@ -1,40 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ChatMessage from "./types/ChatMessage";
 import ChatMessageListItem from "./ChatMessageListItem";
-import profileService from "./services/profileService";
 import UserProfile from "./types/UserProfile";
 
 interface ChatMessageListProps {
+  userProfile: UserProfile;
   messages: ChatMessage[];
 }
-const ChatMessageList: React.FC<ChatMessageListProps> = ({ messages }) => {
-  const [userProfiles, setUserProfiles] = useState<UserProfile[]>([]);
-
-  const fetchUserProfiles = async (userIds: number[]) => {
-    const data = await profileService.fetchUserProfiles({ userIds });
-    setUserProfiles(data);
-  };
-
-  useEffect(() => {
-    const senderIds = messages.map(({ senderId }) => senderId);
-    const uniqueSenderIds = Array.from(new Set(senderIds));
-    if (uniqueSenderIds.length > 0) {
-      fetchUserProfiles(uniqueSenderIds);
-    }
-  }, [messages]);
-
-  const getSenderProfile = (senderId: number) => {
-    return userProfiles.find((p) => p.id === senderId)!;
-  };
-
+const ChatMessageList: React.FC<ChatMessageListProps> = ({
+  userProfile,
+  messages
+}) => {
   return (
     <ol className="ChatMessageList">
       {messages.map(({ id, senderId, text }) => (
-        <ChatMessageListItem
-          key={id}
-          text={text}
-          senderProfile={getSenderProfile(senderId)}
-        />
+        <ChatMessageListItem key={id} text={text} senderProfile={userProfile} />
       ))}
     </ol>
   );
--- src/ChatRoom.tsx
+++ src/ChatRoom.tsx
@@ -1,30 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ChatMessage from "./types/ChatMessage";
 import ChatMessageList from "./ChatMessageList";
 import chatService from "./services/chatService";
+import useDelay from "./hooks/useDelay";
 import useMessageGroup from "./hooks/useMessgeGroup";
+import UserProfile from "./types/UserProfile";
+import profileService from "./services/profileService";
 
 interface ChatRoomProps {
   chatRoomId: number;
 }
 const ChatRoom: React.FC<ChatRoomProps> = ({ chatRoomId }) => {
+  const bottomRef = useRef(null);
   const [rawMessages, setRawMessages] = useState<ChatMessage[]>([]);
-  const messageGroups = useMessageGroup(rawMessages);
+  const [userProfiles, setUserProfiles] = useState<UserProfile[]>([]);
+  const [delayState, delayApi] = useDelay<ChatMessage>(rawMessages);
+  const messageGroups = useMessageGroup(delayState.data);
 
   const fetchChatMessages = async (chatRoomId: number) => {
     const data = await chatService.fetchChatMessages({ chatRoomId });
     setRawMessages(data);
   };
 
+  const fetchUserProfiles = async (userIds: number[]) => {
+    const data = await profileService.fetchUserProfiles({ userIds });
+    setUserProfiles(data);
+  };
+
   useEffect(() => {
     fetchChatMessages(chatRoomId);
   }, [chatRoomId]);
 
+  useEffect(() => {
+    const senderIds = rawMessages.map(({ senderId }) => senderId);
+    const uniqueSenderIds = Array.from(new Set(senderIds));
+    if (uniqueSenderIds.length > 0) {
+      fetchUserProfiles(uniqueSenderIds);
+    }
+  }, [rawMessages]);
+
+  useEffect(() => {
+    bottomRef?.current.scrollIntoView({ behavior: "smooth" });
+  }, [messageGroups]);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    if (delayState.finish) {
+      timeout = setTimeout(() => {
+        delayApi.reset();
+      }, 2000);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [delayState.finish, delayApi]);
+
+  const getSenderProfile = (senderId: number) => {
+    return userProfiles.find((p) => p.id === senderId)!;
+  };
+
   return (
     <div className="ChatRoom">
-      {messageGroups.map((messages) => (
-        <ChatMessageList messages={messages} />
-      ))}
+      {messageGroups.map((messages, i) => {
+        if (messages.length === 0) {
+          return null;
+        }
+        const userProfile: UserProfile = getSenderProfile(messages[0].senderId);
+        return (
+          <ChatMessageList
+            key={`${i}_${userProfile.name}`}
+            userProfile={userProfile}
+            messages={messages}
+          />
+        );
+      })}
+      <div ref={bottomRef} />
     </div>
   );
 };
