7572e57dc3cf79dfe7f8864ee56086c210d58e38
Minu <juty9026@gmail.com>
2021-12-28 15:30:10 +0900

    Create useContinuousGroup.tsx

---
+++ src/hooks/useContinuousGroup.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useContinuousGroup = <T extends unknown>(
+  data: T[],
+  groupBy: keyof T
+): T[][] => {
+  const [groups, setGroups] = useState<T[][]>([]);
+
+  // Lint error (react-hooks/exhaustive-deps)
+  // https://github.com/facebook/react/pull/19751
+  useEffect(() => {
+    const splitted = data.reduce<T[][]>((acc, cur) => {
+      if (acc.length === 0) {
+        acc.push([]);
+      }
+      const target = acc[acc.length - 1];
+      if (target.length === 0 || target[0][groupBy] === cur[groupBy]) {
+        target.push(cur);
+      } else {
+        acc.push([cur]);
+      }
+      return acc;
+    }, []);
+    setGroups(splitted);
+  }, [data, groupBy]);
+
+  return groups;
+};
+
+export default useContinuousGroup;
